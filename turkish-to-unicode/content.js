b29132ebc4d604b98359a25f7a6b43ca8e757b9a
arda.uslu <arda.uslu@sahibinden.com>
2024-08-01 17:49:33 +0300

    convert special chars

--- turkish-to-unicode/content.js
+++ turkish-to-unicode/content.js
@@ -4,26 +4,49 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-function convertToUnicode(inputText) {
-    // Türkçe karakterleri Unicode ile değiştir
-    inputText = inputText.replace(/ğ/g, '\\u011f'); // ğ
-    inputText = inputText.replace(/Ğ/g, '\\u011e'); // Ğ
-    inputText = inputText.replace(/ı/g, '\\u0131'); // ı
-    inputText = inputText.replace(/İ/g, '\\u0130'); // İ
-    inputText = inputText.replace(/ö/g, '\\u00f6'); // ö
-    inputText = inputText.replace(/Ö/g, '\\u00d6'); // Ö
-    inputText = inputText.replace(/ü/g, '\\u00fc'); // ü
-    inputText = inputText.replace(/Ü/g, '\\u00dc'); // Ü
-    inputText = inputText.replace(/ş/g, '\\u015f'); // ş
-    inputText = inputText.replace(/Ş/g, '\\u015e'); // Ş
-    inputText = inputText.replace(/ç/g, '\\u00e7'); // ç
-    inputText = inputText.replace(/Ç/g, '\\u00c7'); // Ç
+function convertToUnicode(inputText, includeSpecialChars, specialChars) {
+    const specialCharMap = {
+        "’": '\\u0027', '+': '\\u002b', '%': '\\u0025', '!': '\\u0021',
+        '"': '\\u0022', '#': '\\u0023', '$': '\\u0024', '&': '\\u0026',
+        '(': '\\u0028', ')': '\\u0029', '*': '\\u002a', ',': '\\u002c',
+        '-': '\\u002d', '.': '\\u002e', '/': '\\u002f', ':': '\\u003a',
+        ';': '\\u003b', '<': '\\u003c', '=': '\\u003d', '>': '\\u003e',
+        '?': '\\u003f', '@': '\\u0040', '[': '\\u005b', '\\': '\\u005c',
+        ']': '\\u005d', '^': '\\u005e', '_': '\\u005f', '`': '\\u0060',
+        '{': '\\u007b', '|': '\\u007c', '}': '\\u007d', '~': '\\u007e', "'": '\\u0027'
+    };
 
-    return inputText;
+    const turkishCharMap = {
+        'ğ': '\\u011f', 'Ğ': '\\u011e', 'ı': '\\u0131', 'İ': '\\u0130',
+        'ö': '\\u00f6', 'Ö': '\\u00d6', 'ü': '\\u00fc', 'Ü': '\\u00dc',
+        'ş': '\\u015f', 'Ş': '\\u015e', 'ç': '\\u00e7', 'Ç': '\\u00c7'
+    };
+
+    // Kullanıcının belirttiği özel karakterleri set olarak saklayın
+    const specialCharsSet = new Set(specialChars.split(''));
+
+    let result = '';
+
+    for (let i = 0; i < inputText.length; i++) {
+        const char = inputText[i];
+
+        if (includeSpecialChars && specialCharsSet.has(char)) {
+            // Eğer özel karakterler korunacaksa ve karakter belirtilmişse, bu karakteri değişmeden bırak
+            result += char;
+        } else {
+            // Türkçe karakterleri ve diğer özel karakterleri dönüştür
+            result += turkishCharMap[char] || (!includeSpecialChars ? char : specialCharMap[char] || char);
+        }
+    }
+
+    return result;
 }
 
 function convertAndDisplay() {
-    var inputText = document.getElementById("inputText").value;
-    var convertedText = convertToUnicode(inputText);
+    const inputText = document.getElementById("inputText").value;
+    const includeSpecialChars = document.getElementById("includeSpecialChars").checked;
+    const specialChars = document.getElementById("specialChars").value;
+
+    const convertedText = convertToUnicode(inputText, includeSpecialChars, specialChars);
     document.getElementById("outputContainer").innerText = convertedText;
 }
